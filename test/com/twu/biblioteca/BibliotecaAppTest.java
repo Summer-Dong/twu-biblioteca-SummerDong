bc227823e15e5ac0590fb975cccfbe7576408c4f
Summer-Dong <D__yanan@163.com>
2017-06-08 10:08:44 +0800

    [#2]booklist test

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,22 +1,26 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
-
+import org.hamcrest.beans.SamePropertyValuesAs;
 import static org.junit.Assert.*;
 
 /**
  * Created by summer on 2017/6/7.
  */
 public class BibliotecaAppTest {
-  @Test
-  public void getBooks() throws Exception {
-  }
-
   BibliotecaApp bibliotecaApp = new BibliotecaApp();
+  private Books[] books = new Books[] {
 
+  };
+
+  //welcome msg test
   @Test
   public void getWelcomeMsg() throws Exception {
     assertEquals("Welcome to Biblioteca!", bibliotecaApp.getWelcomeMsg());
   }
-
+  //show booklist test
+  @Test
+  public void getBooks() throws Exception {
+    assertThat(bibliotecaApp.getBooks(), new SamePropertyValuesAs(books));
+  }
 }
